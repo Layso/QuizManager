0f030238b392902f5d292a66e128ef1bb4ae765a
Layso <laysogame@hotmail.com>
2018-12-26 05:00:44 +0100

    Test function for DatabaseManager created

--- src/com/layso/quizmanager/test/Main.java
+++ src/com/layso/quizmanager/test/Main.java
@@ -1,8 +1,17 @@
 package com.layso.quizmanager.test;
 
+import com.layso.quizmanager.services.*;
 
 public class Main {
 	public static void main(String[] args) {
+		DatabaseTest();
+		
+	}
 	
+	
+	
+	public static void DatabaseTest() {
+		DatabaseManager db = new DatabaseManager("jdbc:h2:~/test", "sa", "");
+		System.out.println(db.Connect());
 	}
 }
